6eb104ec7adcafa2d4a40fc8d301599fe07a2757
Nada-is-here <Dragoordfire557@gmail.com>
2025-02-14 14:31:27 +0000

    Add files via upload
    this simple math functions i created.

---
+++ math functions.py
@@ -0,0 +1,26 @@
+
+a = 1
+b = 2
+
+def addition(a,b):
+    return a+b
+print(addition(a,b))
+def subtract(a,b):
+    return a-b
+print(subtract(a,b))
+
+def division(a,b):
+    if b == 0:
+         return "division of 0 is not possible."
+    else:
+         return a/b,
+
+print(division(a,b))
+def multiplicative(a,b):
+    return a*b
+print(multiplicative(a,b))
+def to_the_power(a,b):
+    return a**b
+print(to_the_power(a,b))
+
+
